14e374dbca663d5bf51fa4a4147eed13ec655117
Joseph Ramli <jramli1@binghamton.edu>
2019-10-16 10:07:35 -0400

    Added storyboard start screen, segue to hello world game view controller

---
+++ project4/project4/StartViewController.swift
@@ -0,0 +1,31 @@
+//
+//  StartViewController.swift
+//  project4
+//
+//  Created by Joseph Daniel Ramli on 10/16/19.
+//  Copyright © 2019 Joseph Daniel Ramli. All rights reserved.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        print("Initial view started")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
